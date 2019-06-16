ada8f0fbdeb0030b83e0be5d60814cc8e6403e3d
hugo costa <hugocosta.oli@gmail.com>
2019-06-15 23:45:27 -0300

    Finalização de Portfolio, novas cores e fonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 $(function () {$('[data-toggle="tooltip"]').tooltip()})
+
+
+$(function(){
+    var body=document.body,
+html=document.documentElement,
+menu=document.querySelector('.cabecalho'),
+menuPosition;
+window.addEventListener('scroll',function(){
+  menuPosition=menu.offsetTop+120;
+  if(body.scrollTop > menuPosition || html.scrollTop > menuPosition){
+    menu.classList.add('ativo');
+  }else{
+    menu.classList.remove('ativo');
+  }
+//      console.log(menu.offsetTop);
+});
+
+});
