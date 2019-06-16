91a3636d997c37aa5797b5026eff8892e0ebcb0a
hugo costa <hugocosta.oli@gmail.com>
2019-06-16 19:14:48 -0300

    Suavização do scrool

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
+//Tooltip bootstrap
 $(function () {$('[data-toggle="tooltip"]').tooltip()})
-
-
+//Diminuindo cabeçalho
 $(function(){
-    var body=document.body,
-html=document.documentElement,
-menu=document.querySelector('.cabecalho'),
-menuPosition;
-window.addEventListener('scroll',function(){
-  menuPosition=menu.offsetTop+120;
-  if(body.scrollTop > menuPosition || html.scrollTop > menuPosition){
-    menu.classList.add('ativo');
-  }else{
-    menu.classList.remove('ativo');
+  var body=document.body,html=document.documentElement,menu=document.querySelector('.cabecalho'),menuPosition;window.addEventListener('scroll',function(){
+    menuPosition=menu.offsetTop+120;
+    if(body.scrollTop > menuPosition || html.scrollTop > menuPosition){
+        menu.classList.add('ativo');
+    }else{
+        menu.classList.remove('ativo');
   }
 //      console.log(menu.offsetTop);
 });
-
 });
-
-$(document).ready(function () {
-  $('body').niceScroll();
-});
+//Deslizamento
+jQuery(document).ready(function($) {
+  $(".scroll").click(function(event){
+      event.preventDefault();
+   $('html,body').animate({
+      scrollTop:$(this.hash).offset().top}, 1000);
+  });
+ });
